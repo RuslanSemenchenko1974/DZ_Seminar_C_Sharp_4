cc4c532fd7b23a8972c1f8542e88318c2202655e
Ruslan <89604587433@mail.ru>
2022-09-10 23:39:32 +0300

    принимает на вход число и выдаёт сумму цифр в числе

---
+++ Task27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int GetSumm(int number)
+{
+    number=Math.Abs(number);
+    int summ=0;
+    while (number!=0)
+    {   
+        summ=summ+number%10;
+        number=number/10;
+    }
+    return summ;
+}
+Console.Clear();
+try
+{
+    Console.WriteLine("Введите число");
+    int number=Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Сумма {GetSumm(number)}");
+}
+catch(Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
